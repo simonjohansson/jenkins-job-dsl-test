76a489ce30c9716b698724db6de4b558a75cdc95
Simon Johansson <simon@simonjohansson.com>
2015-02-03 12:05:16 +0100

    Does it need to be strings?

--- jenkins/flow.groovy
+++ jenkins/flow.groovy
@@ -2,10 +2,10 @@ node('master') {
   git url: 'https://github.com/simonjohansson/jenkins-job-dsl-test'
   doSomething 'a'
   parallel(sleep60: {
-	     sleep 60
+	     sleep "60"
 	   },
 	   sleep50: {
-	     sleep 50
+	     sleep "50"
 	   })
   kehe 'a'
 }
