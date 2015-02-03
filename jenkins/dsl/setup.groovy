3a3c79e667876eb93aca3b9ee902300d354fc1a1
Simon Johansson <simon@simonjohansson.com>
2015-02-03 10:56:16 +0100

    Update scm path to my own repo.

--- jenkins/dsl/setup.groovy
+++ jenkins/dsl/setup.groovy
@@ -1,7 +1,7 @@
 job {
   name 'My-first-awesome-job'
   scm {
-    git('git://github.com/jgritman/aws-sdk-test.git')
+    git('https://github.com/simonjohansson/jenkins-job-dsl-test')
   }
   triggers {
     scm('*/15 * * * *')
