41d8412ddccc0b99dce50521feed774ec99cb3a2
Simon Johansson <simon@simonjohansson.com>
2015-02-03 11:01:36 +0100

    Dont push tags for builds plz.

--- jenkins/dsl/setup.groovy
+++ jenkins/dsl/setup.groovy
@@ -1,7 +1,13 @@
 job {
   name 'My-first-awesome-job'
   scm {
-    git('https://github.com/simonjohansson/jenkins-job-dsl-test')
+    git {
+      remote {
+	url('https://github.com/simonjohansson/jenkins-job-dsl-test')
+      }
+      createTag(false)
+
+    }
   }
   triggers {
     scm('*/15 * * * *')
